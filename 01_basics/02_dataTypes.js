882d6b449dc704e2504ff224e3d7fee0adf78638
Pranjal Rastogi <126326239+pranjal-rastogi@users.noreply.github.com>
2024-03-04 17:34:08 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using node js,not browser
+
+let name = "Pranjal"
+let age = 19
+let isLoggedIn = false
+let state;
+let temperature = null;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true or false
+// undefined 
+// null (standalone value)
+// symbol
+
+console.log(typeof "Pranjal");
+console.log(typeof undefined); // null
+console.log(typeof null); // object
