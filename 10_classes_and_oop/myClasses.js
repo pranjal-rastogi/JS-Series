534b367edb44681267854677a2b55438bbc03e8b
Pranjal Rastogi <126326239+pranjal-rastogi@users.noreply.github.com>
2024-03-08 09:40:32 +0000

    bind keyword in javascript

---
+++ 10_classes_and_oop/myClasses.js
@@ -0,0 +1,34 @@
+// class User {
+//     constructor(username,email,password){
+//         this.username = username
+//         this.email = email
+//         this.password = password
+//     }
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const chai = new User("chai","chai@gmail.com","123")
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUsername());
+
+// behind the scene
+function User(username,email,password){
+    this.username = username
+    this.email = email
+    this.password = password
+}
+User.prototype.encryptPassword = function(){
+    return `${this.passwordssword}abc`
+}
+User.prototype.ChangeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const tea = new User("tea","google@com",123)
+console.log(tea.encryptPassword());
+console.log(tea.ChangeUsername());
